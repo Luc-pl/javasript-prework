f0f6f66cad0daee7ae8affb94b67b117a9cc490a
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-05-07 21:58:32 +0200

    Change if... else if...

--- js/script.js
+++ js/script.js
@@ -22,11 +22,11 @@ if(randomNumber == 1){
   computerMove = 'kamień';
 }
 
-if(randomNumber == 2){
+else if(randomNumber == 2){
   computerMove = 'papier';
 }
 
-if(randomNumber == 3){
+else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
 
@@ -44,11 +44,11 @@ if(playerInput == '1'){
   playerMove = 'kamień';
 }
 
-if(playerInput == '2'){
+else if(playerInput == '2'){
     playerMove = 'papier';
 }
 
-if(playerInput == '3'){
+else if(playerInput == '3'){
     playerMove = 'nożyce'
 }
 
@@ -60,11 +60,11 @@ if( computerMove == 'kamień' && playerMove == 'papier') {
     printMessage('Ty wygrywasz')
 }
 
-if( computerMove == 'papier' && playerMove == 'nożyce') {
+else if( computerMove == 'papier' && playerMove == 'nożyce') {
     printMessage('Ty wygrywasz')
 }
 
-if( computerMove == 'nożyce' && playerMove == 'kamień') {
+else if( computerMove == 'nożyce' && playerMove == 'kamień') {
     printMessage('Ty wygrywasz')
 }
 
@@ -72,11 +72,11 @@ if( computerMove == 'papier' && playerMove == 'kamień') {
     printMessage('Przegrywasz')
 }
 
-if( computerMove == 'nożyce' && playerMove == 'papier') {
+else if( computerMove == 'nożyce' && playerMove == 'papier') {
     printMessage('Przegrywasz')
 }
 
-if( computerMove == 'kamień' && playerMove == 'nożyce') {
+else if( computerMove == 'kamień' && playerMove == 'nożyce') {
     printMessage('Przegrywasz')
 }
 
@@ -86,11 +86,11 @@ if( computerMove == 'papier' && playerMove == 'papier') {
     printMessage('Remis')
 }
 
-if( computerMove == 'nożyce' && playerMove == 'nożyce') {
+else if( computerMove == 'nożyce' && playerMove == 'nożyce') {
     printMessage('Remis')
 }
 
-if( computerMove == 'kamień' && playerMove == 'kamień') {
+else if( computerMove == 'kamień' && playerMove == 'kamień') {
     printMessage('Remis')
 }
 
@@ -100,10 +100,10 @@ if( computerMove == 'papier' && playerMove == 'nieznany ruch') {
     printMessage('Wybierz poprawny numer ruchu')
 }
 
-if( computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
+else if( computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
     printMessage('Wybierz poprawny numer ruchu')
 }
 
-if( computerMove == 'kamień' && playerMove == 'nieznany ruch') {
+else if( computerMove == 'kamień' && playerMove == 'nieznany ruch') {
     printMessage('Wybierz poprawny numer ruchu')
 }
