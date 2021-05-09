3e0ba3e368a7e56d77f269d5860312716f6c7ba4
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-05-09 23:43:20 +0200

    Change code & style

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,12 @@ function printMessage(msg){
 	document.getElementById('messages').appendChild(div);
 }
 
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('result').appendChild(div);
+}
+
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
